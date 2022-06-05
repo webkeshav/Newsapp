a08ae631344b0a72a41b5c2be6a90cd28dda4e34
Keshav <keshavtomar999@gmail.com>
2022-06-06 00:23:29 +0530

    categories wise news

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,75 +1,83 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
-    constructor(){
-        super();
+    static defaultProps = {
+        country : "in",
+        pageSize : 8,
+        category : "general"
+    }
+
+    static propTypes ={
+        country: PropTypes.string,
+        pageSize : PropTypes.number,
+        category: PropTypes.string,
+    }
+
+
+    async updateNews(){
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=19f57aa98cc841dda3cd5c709e495e2e&page=${this.state.page}&pagesize=${this.props.pageSize}`
+        this.setState({loading:true});
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        this.setState({
+            article : parsedData.articles,
+            totalResults : parsedData.totalResults,
+            loading : false
+        })
+    }
+    constructor(props){
+        super(props);
         console.log("Hello I am a constructor from news component");
         this.state = {
             article : [],
-            loading : false,
+            loading :false,
             page : 1
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)}-NewsMonkey`
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=19f57aa98cc841dda3cd5c709e495e2e&page=1&pagesize=20"
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({
-            article : parsedData.articles,
-            totalResults : parsedData.totalResults
-        })
+        this.updateNews();
     }
-
+    capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+      
     handlePrevClick = async () => {
-    
-        let url =  `https://newsapi.org/v2/top-headlines?country=us&apiKey=19f57aa98cc841dda3cd5c709e495e2e&page=${this.state.page -1}&pagesize=20`
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({
-            article : parsedData.articles,
-            page : this.state.page-1
-        })
+        this.setState({page:this.state.page-1});
+        this.updateNews();
 
     }
 
 
     handleNextClick  = async () => {
-        if(this.state.page +1 > Math.ceil(this.state.totalResults/20)){
-
-        }
-        else{
-        let url =  `https://newsapi.org/v2/top-headlines?country=us&apiKey=19f57aa98cc841dda3cd5c709e495e2e&page=${this.state.page +1}&pagesize=20`
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({
-            article : parsedData.articles,
-            page : this.state.page+1 
-    })
+    this.setState({page:this.state.page-1});
+    this.updateNews();
 
     }
-}
+
 
 
   render() {
     return (
       <div className='container my-3'>
-        <h1>this is a news Component.</h1>
+        <h1 className='text-center my-3'>News Monkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines .</h1>
+        {this.state.loading && <Spinner />}
         <div className='row'>
-            {this.state.article.map((element)=>{
+            {!this.state.loading && this.state.article.map((element)=>{
                 return <div className='col-md-4' key={element.url}>
-                <Newsitem title={element.title?element.title.slice(0,45):""} description = {element.description?element.description.slice(0,88):""} imageurl={element.urlToImage} newsurl ={element.url}/>
+                <Newsitem title={element.title?element.title.slice(0,45):""} description = {element.description?element.description.slice(0,88):""} imageurl={element.urlToImage} newsurl ={element.url} author={element.author} date ={element.publishedAt} source={element.source.name}/>
                 </div>
             })}
         
         </div>
         <div className='container my-3 d-flex justify-content-between'>
         <button type="button" disabled={this.state.page <=1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-        <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        <button type="button" disabled={this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
-      let {title,description,imageurl,newsurl} = this.props;
+      let {title,description,imageurl,newsurl,author,date,source} = this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left :"90%",zIndex:1}}>
+              {source}
+            </span>
         <img src={!imageurl?"https://cdn.vox-cdn.com/thumbor/zNWkJCpbCysQCHjPFJGfJ10m6wE=/0x130:2560x1470/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/23606365/LG_OLED_Objet_Collection_POSE_PR_1.jpg":imageurl} className="card-img-top" alt="..." />
         <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
+            
             <a href={newsurl} target="_blank" rel="noreferrer" className="btn btn-dark">Read More</a>
         </div>
         </div>
